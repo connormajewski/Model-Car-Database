182019bc2924a48e5652730857387897a20cbd9b
connormajewski <cmajews2@gmail.com>
2025-01-17 15:30:53 -0500

    First function version of catalogue application.

--- backend.py
+++ backend.py
@@ -53,21 +53,71 @@ def execute_query(query, params=None):
         return []
 
 
-def add_model(brand, year, description, scale, condition, quantity, value):
-    insertion_query = f"INSERT INTO models (brand, year, description, scale, condition, quantity, value) VALUES('{brand}', {year}, '{description.replace("'", "''")}', '{scale}', '{condition}', {quantity}, {value})"
+def add_model(brand, year, scale, condition, quantity, description='', value=0.0):
+    
+    attr = ["brand", "year", "scale", "condition", "quantity"]
+    attr_value = [brand, int(year), scale, condition, int(quantity)]
+    placeholders = ["?", "?", "?", "?", "?"]
+    
+    if description != '':
+        attr.append("description")
+        attr_value.append(f"'description'")
+        placeholders.append("?")
+        
+    
+    attr.append("value")
+    attr_value.append(0.0 if value == '' else float(value))
+    placeholders.append("?")
+        
+    attr_string = ", ".join(attr)
+    print(attr)
+    placeholders_string  = ", ".join(placeholders)
+    query = f"INSERT INTO models ({attr_string}) VALUES ({placeholders_string})"
+    
+    print(query)
+    print(attr_value)
+    
     try:
-        execute_query(insertion_query)
+        execute_query(query, tuple(attr_value))
     except sqlite3.Error as error:
         print(f"Error inserting model: {error}")
+        
+def update_model(model_id, brand=None, year=None, scale=None, condition=None, quantity=None, description=None, value=None):
     
-"""    
-def query_all_models():
-    selection_query = "SELECT * FROM models ORDER BY id"
-    results = execute_query(selection_query)
-    return results
+    attr=["brand", "year", "scale", "condition", "quantity", "description", "value"]
+    attr_value=[brand, year, scale, condition, quantity, description, value]
     
-def query_total_value():
-    selection_query = "SELECT SUM(value) FROM models"
-    result = execute_query(selection_query)
-    return result
-"""
+    query_values = []
+    query_attr = []
+    
+    for i in range(len(attr)):
+        if(attr_value[i] != ''):
+            if attr[i] == 'year' or attr_value[i] == 'quantity':
+                query_attr.append(attr[i])
+                query_values.append(int(attr_value[i]))
+            elif attr[i] == 'value':
+                query_attr.append(attr[i])
+                query_values.append(float(attr_value[i]))
+            else:
+                query_attr.append(attr[i])
+                query_values.append(f"'{attr_value[i]}'")
+
+   
+    
+    q = ", ".join([f"{a} = {b}" for a, b in zip(query_attr, query_values)])
+   
+    query=f"UPDATE models SET {q} WHERE id is {model_id}"
+ 
+    try:
+        execute_query(query)
+    except sqlite3.Error as error:
+        print(f"Error deleting model: {error}") 
+ 
+def delete_model(model_id):
+    try:
+        query = f"DELETE FROM models WHERE id is {model_id}"
+        print(query)
+        execute_query(query)
+    except sqlite3.Error as error:
+        print(f"Error deleting model: {error}")  
+   
--- frontend.py
+++ frontend.py
@@ -161,7 +161,7 @@ class Sidebar(ctk.CTkFrame):
         
         self.search_button = ctk.CTkButton(
             self,
-            text="Search",
+            text="Search Catalogue",
             width=width,
             fg_color="#4ab1ff",
             hover_color="#286b9e",
@@ -182,6 +182,8 @@ class Sidebar(ctk.CTkFrame):
             row=11,
         )
         
+        # Export Button
+        
         self.export_button = ctk.CTkButton(
             self,
             text="Export Results",
@@ -198,13 +200,31 @@ class Sidebar(ctk.CTkFrame):
             row=13,
             pady=(10,0)
         )
+        
+        # Edit Button
+        
+        self.edit_button = ctk.CTkButton(
+            self,
+            text="Edit Catalogue",
+            width=width,
+            fg_color="#4ab1ff",
+            hover_color="#286b9e",
+            font=self.winfo_toplevel().button_font,
+            command=lambda: self.edit_catalogue()
+        )
+        
+        self.edit_button.grid(
+            column=0,
+            row=14,
+            pady=(10,0)
+        )
 
     def search_request(self, *args):
         #print(f"{scale}, {value}, {condition}, {brand}, {year}, {minQ}, {maxQ}")
         
         attributes=["scale", "value", "condition", "brand", "year", "min", "max"]
         
-        print(args)
+        #print(args)
         
         arguments={}
         
@@ -228,7 +248,7 @@ class Sidebar(ctk.CTkFrame):
         for key in arguments:
             if arguments[key] is not None:
                 if(key == "value"):
-                    print(arguments[key])
+                    #print(arguments[key])
                     search_query = search_query + f"\nAND {key} BETWEEN {arguments[key][0]} AND {arguments[key][1]}"
                    
                 else:
@@ -248,13 +268,13 @@ class Sidebar(ctk.CTkFrame):
         self.master.main_window.display_query(self.master.queryresults)
         
         if len(results) > 0:
-            self.export_button.configure(state="normal")
+            self.export_button.configure(state="enabled")
         else:
             self.export_button.configure(state="disabled")
             
     def export_results(self):
         
-        query_valuee = 0.0
+        query_value = 0.0
         
         saveFile = filedialog.asksaveasfilename(defaultextension='.xlsx', filetypes=[("Excel - *.xlsx", "*.xlsx")])
         
@@ -269,14 +289,23 @@ class Sidebar(ctk.CTkFrame):
             
         for result in self.master.queryresults:
             
-            query_valuee = query_valuee + result[7]
+            if result[7] is not None:
             
-            work_sheet.append(result)
+                query_value = query_value + (result[7])
+            
+                work_sheet.append(result)
                 
-        work_sheet['J2'] = f"${query_valuee}"
+        work_sheet['J2'] = f"${query_value}"
                 
         work_book.save(saveFile)
-  
+        
+    def edit_catalogue(self):
+        edit_window = EditWindow(self)
+        
+        self.edit_button.configure(state='disabled')
+        self.search_button.configure(state='disabled')
+        self.export_button.configure(state='disabled')
+    
         
 class QueryWindow(ctk.CTkFrame):
     def __init__(self, master, width, height):
@@ -284,39 +313,225 @@ class QueryWindow(ctk.CTkFrame):
         
         self.configure(width=width, height=height, border_width=3, border_color="red")
 
+class EditWindow(ctk.CTkToplevel) :
+    def __init__(self,master):
+        super().__init__(master)
+        
+        self.attribute = ['ID', "Brand", "Year", "Description", "Quantity", "Value", "Scale", "Condition"]
+        
+        self.attribute_list = []
+        
+        self.button_font = ctk.CTkFont(
+            family="Helvetica",
+            size=18
+        )
+        
+        width=250
+        height=450
+        
+        self.title('Edit Catalogue')
+        self.geometry(f'{width}x{height}')
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=1)
+        
+        self.attributes('-topmost', True)
+        
+        self.resizable(False, False)
+        
+        for i in range(len(self.attribute) - 2):
+           
+            self.attribute_textbox = ctk.CTkEntry(
+                self,
+                placeholder_text=f'{self.attribute[i]}',
+                width=width,
+                height=30,
+            )
+            
+            self.attribute_textbox.grid(
+                column=0,
+                row=i,
+                pady=(0,10)
+            )
+            
+            self.attribute_list.append(self.attribute_textbox)
+            
+        self.scale_menu= ctk.CTkComboBox(
+            self,
+            width=width,
+            height=30,
+            values=["1:12", "1:18", "1:24", "1:34", "1:36", "1:48"]
+        )
+        
+        self.scale_menu.grid(
+            column=0,
+            row=7,
+        )
+        
+        self.condition_menu= ctk.CTkComboBox(
+            self,
+            width=width,
+            height=30,
+            values=["sealed", "opened"]
+        )
+        
+        self.condition_menu.grid(
+            column=0,
+            row=8,
+        )
+
+        self.attribute_list.append(self.scale_menu)
+        self.attribute_list.append(self.condition_menu)         
+        
+        self.add_button = ctk.CTkButton(
+            self,
+            text="Add Model",
+            width=width,
+            fg_color="#4ab1ff",
+            hover_color="#286b9e",
+            font=self.button_font,
+            command=lambda: backend.add_model(
+                self.attribute_list[1].get(),
+                self.attribute_list[2].get(),
+                self.scale_menu.get(),
+                self.condition_menu.get(),
+                self.attribute_list[4].get(),
+                self.attribute_list[3].get(),
+                self.attribute_list[5].get()
+            )
+        )
+        
+        self.add_button.grid(
+            column=0,
+            row=9,
+            pady=(10,0)
+        )
+        
+        self.update_button = ctk.CTkButton(
+            self,
+            text="Update Model",
+            width=width,
+            fg_color="#4ab1ff",
+            hover_color="#286b9e",
+            font=self.button_font,
+            command=lambda: backend.update_model(
+                self.attribute_list[0].get(),
+                self.attribute_list[1].get(),
+                self.attribute_list[2].get(),
+                self.scale_menu.get(),
+                self.condition_menu.get(),
+                self.attribute_list[4].get(),
+                self.attribute_list[3].get(),
+                self.attribute_list[5].get()
+            )
+        )
+        
+        self.update_button.grid(
+            column=0,
+            row=10,
+            pady=(10,0)
+        )
+        
+        self.delete_button = ctk.CTkButton(
+            self,
+            text="Delete Model",
+            width=width,
+            fg_color="#4ab1ff",
+            hover_color="#286b9e",
+            font=self.button_font,
+            command = lambda: backend.delete_model(self.attribute_list[0].get())
+        )
+        
+        self.delete_button.grid(
+            column=0,
+            row=11,
+            pady=(10,0)
+        )
+        
+        self.protocol('WM_DELETE_WINDOW', self.close)
+        
+    def close(self):
+        
+        self.master.edit_button.configure(state='enabled')
+        self.master.search_button.configure(state='enabled')
+        self.master.export_button.configure(state='enabled')
+        
+        self.destroy()
+
  
 class MainWindow(ctk.CTkScrollableFrame):
     def __init__(self, master, width, height):
         super().__init__(master)
         
-        self.models = []
+        self.master = master
         
-        #, border_width=3, border_color="red"
+        self.models = []
         
         self.configure(width=width, height=height)
         
-        # self.querywindow = QueryWindow(self, width, height)
-        # self.querywindow.pack(pady=0, padx=0)
-        
     def display_query(self, queryresults) -> list:
+        
+        attrs = ["Id", "Brand", "Year", "Scale", "Condition", "Quantity", "Value"]
         
         if len(self.models) > 0:
             for i in range(len(self.models)):
                 self.models[i].destroy()
+        
+        for i in range(len(attrs)):
+        
+            self.delete_button = ctk.CTkButton(
+                self,
+                text=attrs[i],
+                width=100,
+                fg_color="#4ab1ff",
+                hover_color="#286b9e",
+                command=lambda i=i : self.filter_results(queryresults, i+1 if i > 2 else i)
+            )
+            
+            self.delete_button.grid(
+                column=11,
+                row=i+1,
+                pady=(10,0),
+                padx=(20,0)
+            )
+          
+            
                 
         label = ctk.CTkLabel(self, text=f"{'ID' : >4}: {'BRAND'  : <12} {'YEAR' : <4} {'SCALE' : <5} {'CONDITION' : <9} {'QUANTITY' : <8} {'VALUE' : <5}", font=("Courier", 16))
         
-        label.grid(row=0, column=0, pady=(0,5), sticky="w")
+        label.grid(row=1, column=0, pady=(0,5), sticky="w")
 
-        for j in range(1, len(queryresults)):
+        for j in range(1, len(queryresults) + 1):
            
             i = j - 1
            
-            label = ctk.CTkLabel(self, text=f"{queryresults[i][0] : >4}: {queryresults[i][1]  : <12} {queryresults[i][2] : <4} {queryresults[i][4] : <5} {queryresults[i][5] : <9} {queryresults[i][6] : <8} ${queryresults[i][7] : <5}", font=("Courier", 16))
+            label = ctk.CTkLabel(self, text=f"{queryresults[i][0] : >4}: {queryresults[i][1]  : <12} {queryresults[i][2] : <4} {queryresults[i][4] : <5} {queryresults[i][5] : <9} {queryresults[i][6] : <8} ${queryresults[i][7] if queryresults[i][7] != 0.0 else 'N/A' : <5}", font=("Courier", 16))
             
             self.models.append(label)     
             
             label.grid(row=j+1, column=0, pady=(0,5), sticky="w")
+            
+    def edit_database(self):
+        for i in range(5):
+            self.brand_textbox = ctk.CTkEntry(
+                self,
+                placeholder_text="Brand",
+                width=width,
+                height=30,
+            )
+            
+            self.brand_textbox.grid(
+                column=0,
+                row=i,
+                pady=(0,10)
+            )
+            
+    def filter_results(self, results, attr):
+        
+        results = sorted(results, key=lambda x: x[attr])
+        
+        self.master.queryresults  = results
+        
+        self.display_query(self.master.queryresults)
             
 # Main App Class. Variables stored here are able to be used by all children of the class.            
 
